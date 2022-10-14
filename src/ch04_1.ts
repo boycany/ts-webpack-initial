16e27058ec028d7855dd843d3cd99db531def8d0
boycany <yurayurateiko@gmail.com>
2022-10-14 20:20:39 +0800

    ch04-1 start

---
+++ src/ch04_1.ts
@@ -0,0 +1,15 @@
+//type 自定義類型
+
+let num1: number
+let str1: string 
+
+type LiveName = string | boolean | number
+
+let livename: LiveName
+let livename1: LiveName
+let livename2: LiveName
+
+//object
+type OBJ = { name: string, age: number } 
+
+let obj: OBJ
